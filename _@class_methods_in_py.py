209ea4f69ec13fcd8ac38264bbcfaa0332be959a
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-06-21 14:20:58 +0530

    Create _@class_methods_in_py.py

---
+++ _@class_methods_in_py.py
@@ -0,0 +1,19 @@
+class MyClass:
+    class_variable = 0
+    
+    @classmethod
+    def class_method(cls, value):
+        cls.class_variable += value
+        return cls.class_variable
+
+# Calling the class method directly using the class name
+# Llamando al método de clase directamente usando el nombre de la clase
+result = MyClass.class_method(5)
+print(result)  # Output will be 5
+
+result = MyClass.class_method(3)
+print(result)  # Output will be 8
+
+#To call a class method you don't need to create an instance of the class. 
+# Instead, just use the class name, followed by a dot and the class method name.
+
