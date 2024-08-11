8119367254e07bfc15dd75e6208188a2e83e5ab9
R.K. Janith Prabash <janithprabash944ugc@gmail.com>
2024-08-11 18:34:40 +0530

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,10 @@
+
+print("Output")
+# A message for the user
+message = "This is my message"
+note = "Please proceed"
+print(message) 
+
+# Perform mathematical operation
+result = 3*3
+print(result)
